2290c66ae3edc8503a060d8c782dbe62f6ea9629
Gabriel Navas <gabnavas.dev@gmail.com>
2025-07-03 02:38:24 -0300

    fix: get all with empty list

--- todo/api/usecases/todos/get_all_todo.go
+++ todo/api/usecases/todos/get_all_todo.go
@@ -16,19 +16,19 @@ func NewGetAllTodosUsecase(db *gorm.DB) *GetAllTodosUsecase {
 	return &GetAllTodosUsecase{db: db}
 }
 
-func (u *GetAllTodosUsecase) GetAllTodos(ctx *gin.Context, offset int, limit int) ([]dto.TodoResponse, error) {
-	var todos []models.Todo
-	u.db.Offset(offset).Limit(limit).Find(&todos)
+func (u *GetAllTodosUsecase) GetAllTodos(ctx *gin.Context, offset int, limit int) ([]*dto.TodoResponse, error) {
+	var todos []*models.Todo
+	u.db.Offset(offset).Limit(limit).Order("created_at DESC").Find(&todos)
 
-	var todoResponses []dto.TodoResponse
-	for _, todo := range todos {
-		todoResponses = append(todoResponses, dto.TodoResponse{
+	var todoResponses []*dto.TodoResponse = make([]*dto.TodoResponse, len(todos))
+	for i, todo := range todos {
+		todoResponses[i] = &dto.TodoResponse{
 			ID:        todo.ID,
 			Title:     todo.Title,
 			Completed: todo.Completed,
 			CreatedAt: todo.CreatedAt,
 			UpdatedAt: todo.UpdatedAt,
-		})
+		}
 	}
 
 	return todoResponses, nil
